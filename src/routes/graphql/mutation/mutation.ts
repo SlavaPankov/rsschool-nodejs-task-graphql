c2a1473666033f0672b288c9b4de199b7a91a153
spankov <spankov@ikod.kz>
2024-11-01 17:55:44 +0500

    feat: implmenet userQuery

---
+++ src/routes/graphql/mutation/mutation.ts
@@ -0,0 +1,7 @@
+import { GraphQLObjectType } from 'graphql';
+import { Context } from '../context.js';
+
+export const typeMutation = new GraphQLObjectType<null, Context>({
+  name: 'Mutation',
+  fields: () => ({}),
+});
