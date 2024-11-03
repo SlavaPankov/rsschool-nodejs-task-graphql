a55aa4dfe17c605e07f6d155d32ab55307a508b0
spankov <spankov@ikod.kz>
2024-11-03 18:02:56 +0500

    feat: implement profiles query

---
+++ src/routes/graphql/points/profiles/query/profiles.ts
@@ -0,0 +1,9 @@
+import { GraphQLFieldConfig, GraphQLNonNull, GraphQLList } from 'graphql';
+import { Context } from '../../../context.js';
+import { typeProfile } from '../typeProfile.js';
+
+export const profilesQuery: GraphQLFieldConfig<unknown, Context> = {
+    type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(typeProfile))),
+
+    resolve: async (_, _arg, ctx: Context) => ctx.prisma.profile.findMany(),
+};
