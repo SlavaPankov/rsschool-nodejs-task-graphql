6de8766b15c52b76da728071fd8ec9df1f55aa98
spankov <spankov@ikod.kz>
2024-11-03 18:00:25 +0500

    feat: implement post query

--- src/routes/graphql/points/posts/typePosts.ts
+++ src/routes/graphql/points/posts/typePosts.ts
@@ -5,14 +5,28 @@ import { Context } from '../../context.js';
 import { typePerson } from '../users/typePerson.js';
 
 export const typePost = new GraphQLObjectType<Post, Context>({
-  name: 'Post',
-  fields: () => ({
-    id: { type: new GraphQLNonNull(UUIDType) },
-    title: { type: new GraphQLNonNull(GraphQLString) },
-    content: { type: new GraphQLNonNull(GraphQLString) },
-    author: {
-      type: new GraphQLNonNull(typePerson),
-      resolve: async (post, _, ctx) => ctx.fetchUsersById.load(post.authorId),
-    },
-  }),
-});
+    name: 'Post',
+    description: 'Represents a blog post or article written by a user',
+    fields: () => ({
+        id: {
+            type: new GraphQLNonNull(UUIDType),
+            description: 'The unique identifier of the post',
+        },
+
+        title: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: 'The title of the post',
+        },
+
+        content: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: 'The content body of the post',
+        },
+        author: {
+            type: new GraphQLNonNull(typePerson),
+            description: 'The user who authored the post',
+            resolve: async (p, _, ctx) => ctx.fetchUsersById.load(p.authorId),
+        },
+
+    }),
+})
